f2b18a014fa60d1ed07bf2c9acc5032ca16480e6
Suna Ulus <suna.ulus@gmail.com>
2022-07-06 16:49:13 -0500

    Third bonus has been solved

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ window.addEventListener("load", function(){
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
         response.json().then(function(json){
             const div= document.getElementById("container");
+            let counter =0;
             for(let index=0; index<json.length; index++){
-
+                    if(json[index].active){
+                        active = "green";
+                    }
                 sortedJson = json.sort((a,b) =>(a.hoursInSpace-b.hoursInSpace))                               
                 div.innerHTML +=
             
@@ -13,12 +16,18 @@ window.addEventListener("load", function(){
                <h3>${json[index].firstName} ${json[index].lastName}</h3>
                <ul>
                   <li>Hours in space: ${json[index].hoursInSpace}</li>
-                  <li>Active: ${json[index].active}</li>
+                  <li id="green">Active: ${json[index].active}</li>
                   <li>Skills: ${json[index].skills}</li>
                </ul>
             </div>
             <img class="avatar" src="${json[index].picture}">
          </div>`
+         counter+=1;
+         let printCrewCount = document.getElementById("crewCount");
+         printCrewCount.innerHTML = `
+         <h3>Astronaut Count: ${counter}</h3>
+         `
+
             };
         });
     });
